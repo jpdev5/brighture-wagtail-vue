7338f823456b80f1c044e7d3bbb5461351476da9
Philip <philip@brightures-MacBook-Air.local>
2025-07-04 16:32:56 +0800

    chore: last backend commit

--- backend/home/models.py
+++ backend/home/models.py
@@ -1,16 +1,25 @@
 from django.db import models
-
 from wagtail.models import Page
 from wagtail.admin.panels import FieldPanel
+from wagtail.images.models import Image
 from wagtail.api import APIField
 
 class HomePage(Page):
-    banner_text = models.TextField(blank = True)
-    
+    banner_text = models.TextField(blank=True)
+    banner_image = models.ForeignKey(
+        Image,
+        null=True,
+        blank=True,
+        on_delete=models.SET_NULL,
+        related_name='+'
+    )
+
     content_panels = Page.content_panels + [
-        FieldPanel("banner_text")
+        FieldPanel("banner_text"),
+        FieldPanel("banner_image"),
     ]
-    
+
     api_fields = [
         APIField("banner_text"),
+        APIField("banner_image"),
     ]
