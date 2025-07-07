e7889944aaf0d2a90c1dd5e87ff007940959bdba
Philip <philip@brightures-MacBook-Air.local>
2025-07-07 10:41:10 +0800

    feat: add avatar icon chore: furnish header

---
+++ frontend/src/components/AppHeader.vue
@@ -0,0 +1,31 @@
+<template>
+  <q-header class="q-px-lg custom-header flex justify-between items-center">
+    <q-tabs no-caps align="left" class="text-dark">
+      <img src="/brighture_bk.png" alt="brighture logo" class="cursor-pointer" />
+      <q-route-tab to="/#" label="Page One" />
+      <q-route-tab to="/#" label="Page Two" />
+      <q-route-tab to="/#" label="Page Three" />
+    </q-tabs>
+
+    <div>
+      <q-btn flat round dense>
+        <q-avatar size="40px">
+          <img src="https://cdn.quasar.dev/img/avatar.png" />
+        </q-avatar>
+        <q-menu>
+          <q-list style="min-width: 150px">
+            <q-item clickable v-close-popup>
+              <q-item-section>Profile</q-item-section>
+            </q-item>
+            <q-item clickable v-close-popup>
+              <q-item-section>Settings</q-item-section>
+            </q-item>
+            <q-item clickable v-close-popup>
+              <q-item-section>Logout</q-item-section>
+            </q-item>
+          </q-list>
+        </q-menu>
+      </q-btn>
+    </div>
+  </q-header>
+</template>
--- frontend/src/layouts/MainLayout.vue
+++ frontend/src/layouts/MainLayout.vue
@@ -1,17 +1,13 @@
 <template>
   <q-layout view="hHh lpR fFf">
-    <q-header class="q-px-lg">
-      <q-tabs dense no-caps align="left" class="text-dark">
-        <img src="/brighture_bk.png" alt="brighture logo" class="cursor-pointer" />
-
-        <q-route-tab to="/#" label="Page One" />
-        <q-route-tab to="/#" label="Page Two" />
-        <q-route-tab to="/#" label="Page Three" />
-      </q-tabs>
-    </q-header>
+    <AppHeader />
 
     <q-page-container>
       <router-view />
     </q-page-container>
   </q-layout>
 </template>
+
+<script setup lang="ts">
+import AppHeader from 'src/components/AppHeader.vue';
+</script>
