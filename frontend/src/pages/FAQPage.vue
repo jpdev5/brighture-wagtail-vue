e9d26a739ba432031937cba9c61528c202cb0b8c
Philip <philip@brightures-MacBook-Air.local>
2025-07-09 09:20:44 +0800

    chore: add tailwind class

--- frontend/src/pages/FAQPage.vue
+++ frontend/src/pages/FAQPage.vue
@@ -34,7 +34,7 @@
     <div class="flex items-center column q-gutter-md">
       <h5>FAQs Content</h5>
 
-      <div class="q-gutter-md" style="display: grid; grid-template-columns: repeat(4, 1fr)">
+      <div class="grid grid-cols-2 gap-3">
         <div v-for="i in 8" :key="i">
           <div class="bg-grey-4 q-pa-sm q-px-xl text-center">FAQ Number: {{ i }}</div>
         </div>
@@ -42,11 +42,23 @@
     </div>
 
     <div class="flex items-center column q-gutter-md">
-      <h5>Frequently Asked Questions Lists</h5>
-
-      <div v-for="i in 5" :key="i">
-        <div class="bg-grey-4 q-pa-sm q-px-xl text-center">FAQ Number: {{ i }}</div>
-      </div>
+      <q-list bordered>
+        <q-expansion-item
+          group="somegroup"
+          icon="explore"
+          label="First"
+          default-opened
+          class="w-72"
+        >
+          <q-card>
+            <q-card-section>
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quidem, eius reprehenderit
+              eos corrupti commodi magni quaerat ex numquam, dolorum officiis modi facere maiores
+              architecto suscipit iste eveniet doloribus ullam aliquid.
+            </q-card-section>
+          </q-card>
+        </q-expansion-item>
+      </q-list>
     </div>
   </div>
 </template>
